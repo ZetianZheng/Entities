156cd4b58308b6b6da2838be5db5d993fd737818
ZetianZheng <ztn.zheng@gmail.com>
2022-03-01 19:01:45 -0500

    init

---
+++ src/main/java/com/udacity/EntityExec/EntityExecApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.EntityExec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityExecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityExecApplication.class, args);
+	}
+
+}
